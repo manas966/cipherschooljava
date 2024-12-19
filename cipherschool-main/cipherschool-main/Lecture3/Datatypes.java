d715aee0b9d9433f3b3b6bf04815592d972abd15
manas966 <121108503+manas966@users.noreply.github.com>
2024-12-19 22:45:53 +0530

    Add files via upload

---
+++ cipherschool-main/cipherschool-main/Lecture3/Datatypes.java
@@ -0,0 +1,19 @@
+package Lecture3;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        System.out.println("X is:" + x);
+        System.out.println("Y is: "+ y);
+
+        byte b = 10;
+        System.out.println("B is: "+ b);
+        
+        byte b2 = 120;
+        System.out.println("B2 is :"+b2);
+
+        int i = 45;
+        System.out.println("I is: "+i);;
+    }
+}
---
+++ cipherschool-main/cipherschool-main/Lecture9/inheritence.java
@@ -0,0 +1,22 @@
+package Lecture9;
+
+class Shape{
+    double pi = 3.14;
+    double getSquare(int r){
+        return r*r;
+    }
+}
+
+class Circle extends Shape{
+    void area(int r){
+        System.out.println(pi*getSquare(r));
+    }
+}
+
+public class inheritence {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.area(5);
+    }
+    
+}
